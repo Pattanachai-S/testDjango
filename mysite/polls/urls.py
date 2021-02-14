6df430556261112d0307749e690be22abee6d812
non <s5901012610082@email.kmutnb.ac.th>
2021-02-14 14:56:19 +0700

    use template and removing hard code url

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,12 +5,20 @@ from . import views
 urlpatterns = [
     # self test
     path('index2', views.index2, name='hello'),
+    
     # ex: /polls/
     path('', views.index, name='index'),
+    
     # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     path('<int:question_id>/', views.detail, name='detail'),
+    
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
+    
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    
 ]
+
