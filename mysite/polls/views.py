80f77e7f2d00921fb3e771e536eb2e49df914328
non <s5901012610082@email.kmutnb.ac.th>
2021-02-14 14:34:54 +0700

    finish raising a 404 error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+''' a full of index (r)
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -11,13 +20,12 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-    
+'''
     
 def index2(request):
     return HttpResponse("Hello .")
     
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -25,3 +33,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+    
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
